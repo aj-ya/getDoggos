41cc90272a446a4a3f4265b18321f25fbe2dcbcc
AJeya Bhat <ajeyabhat.off@gmail.com>
2020-12-18 13:43:56 +0530

    added initial draft

---
+++ main.js
@@ -0,0 +1,9 @@
+addDoggo = () =>
+{
+let newdoggo=document.createElement("div");
+document.getElementById("container").appendChild(newdoggo);
+newdoggo.classList.add("doggo-box");
+let randomdoggo=document.createElement("img");
+newdoggo.appendChild(randomdoggo);
+randomdoggo.src='https://picsum.photos/200/200';
+}
