f14ae1a04a71521c770b29f305e2a736e0d71e8e
AJeya Bhat <ajeyabhat.off@gmail.com>
2020-12-19 15:22:20 +0530

    created wireframe

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ let randomdoggo=document.createElement("img");
 newdoggo.appendChild(randomdoggo);
 randomdoggo.src='https://picsum.photos/200/200';
 }
+deleteAllDoggos = () =>
+{
+    document.getElementById("container").innerHTML= "";
+}
