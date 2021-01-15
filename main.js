620a691129a5745e7f25cf7a98f7f12f0689e244
AJeya Bhat <ajeyabhat.off@gmail.com>
2021-01-15 14:06:56 +0530

    added promises and api

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-addDoggo = () =>
+var src;
+addDoggo = (url) =>
 {
 let newdoggo=document.createElement("div");
 document.getElementById("container").appendChild(newdoggo);
 newdoggo.classList.add("doggo-box");
 let randomdoggo=document.createElement("img");
 newdoggo.appendChild(randomdoggo);
-randomdoggo.src='https://picsum.photos/200/200';
+randomdoggo.width=200;
+randomdoggo.height=200;
+function display(data){
+    data = data.json();
+    return data;
+  }
+const futureData = fetch('https://dog.ceo/api/breeds/image/random');
+futureData.then(display).then( abc=>randomdoggo.src=abc.message);
 }
 deleteAllDoggos = () =>
 {
