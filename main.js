4db6b5f2322685245b5ec76127c145f2a45faeee
AJeya Bhat <ajeyabhat.off@gmail.com>
2021-02-25 17:48:32 +0530

    Final Folder Uploaded -ajeya

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-addDoggo = (url) =>
-{
-let newdoggo=document.createElement("div");
-document.getElementById("container").appendChild(newdoggo);
-newdoggo.classList.add("doggo-box");
-let randomdoggo=document.createElement("img");
-newdoggo.appendChild(randomdoggo);
-randomdoggo.width=200;
-randomdoggo.height=200;
-function display(data){
-    data = data.json();
-    return data;
-  }
-const futureData = fetch('https://dog.ceo/api/breeds/image/random');
-futureData.then(display).then(abc=>randomdoggo.src=abc.message);
-}
-deleteAllDoggos = () =>
-{
-    document.getElementById("container").innerHTML= "";
-}
+addDoggo = (url) => {
+  let newdoggo = document.createElement("div");
+  document.getElementById("container").appendChild(newdoggo);
+  newdoggo.classList.add("doggo-box");
+  let randomdoggo = document.createElement("img");
+  newdoggo.appendChild(randomdoggo);
+  randomdoggo.width = 200;
+  randomdoggo.height = 200;
+  const futureData = fetch("https://dog.ceo/api/breeds/image/random");
+  futureData
+    .then((data) => {
+      data = data.json();
+      return data;
+    })
+    .then((abc) => (randomdoggo.src = abc.message));
+};
+deleteAllDoggos = () => {
+  document.getElementById("container").innerHTML = "";
+};
